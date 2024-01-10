9d97f36e7d74661f4cb3c7b4af95978008143d8c
hsuush <dlhyes703@naver.com>
2024-01-10 23:01:46 +0900

    feat: 유저아이디로 유저 팔로우

---
+++ src/main/java/com/example/ReviewZIP/domain/follow/Follows.java
@@ -0,0 +1,29 @@
+package com.example.ReviewZIP.domain.follow;
+
+import com.example.ReviewZIP.domain.user.Users;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "follows")
+public class Follows {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id")
+    private Users sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id")
+    private Users receiver;
+}
---
+++ src/main/java/com/example/ReviewZIP/domain/user/Users.java
@@ -0,0 +1,48 @@
+package com.example.ReviewZIP.domain.user;
+
+import com.example.ReviewZIP.domain.follow.Follows;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = true)
+    private String profile_image;
+
+    @Column(nullable = false)
+    private String user_id;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String phone_num;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @OneToMany(mappedBy = "sender", cascade = CascadeType.ALL)
+    List<Follows> followingList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "receiver", cascade = CascadeType.ALL)
+    List<Follows> followerList = new ArrayList<>();
+}
