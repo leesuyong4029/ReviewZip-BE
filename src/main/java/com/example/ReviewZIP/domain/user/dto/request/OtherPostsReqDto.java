f75b01284f1c568a16cf6c2b5f73c6e2c6f500d8
hsuush <dlhyes703@naver.com>
2024-01-16 00:44:12 +0900

    feat : 특정 유저의 게시글들 가져오기

---
+++ src/main/java/com/example/ReviewZIP/domain/user/dto/request/OtherPostsReqDto.java
@@ -0,0 +1,7 @@
+package com.example.ReviewZIP.domain.user.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class OtherPostsReqDto {
+}
---
+++ src/main/java/com/example/ReviewZIP/domain/user/dto/response/OtherPostsResDto.java
@@ -0,0 +1,36 @@
+package com.example.ReviewZIP.domain.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class OtherPostsResDto {
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class PostPreviewListDto{
+        private List<PostPreviewDto> postList;
+        private Integer listSize;
+        private Integer totalPage;
+        private Long totalElements;
+        private Boolean isFirst;
+        private Boolean isLast;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class PostPreviewDto{
+        private Long postId;
+        private List<String> imageUrl;
+        private String comment;
+        private Double point;
+        private Integer likesNum;
+    }
+}
