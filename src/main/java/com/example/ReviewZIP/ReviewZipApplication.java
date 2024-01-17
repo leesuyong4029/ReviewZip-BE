9de6098d0228c0a467b0e76ad7221f105c4f4bbf
daeng <89900251+yoondaeng@users.noreply.github.com>
2024-01-17 21:21:07 +0900

    fix: Update ReviewZipApplication.java
    error 수정
    add exclude = SecurityAutoConfiguration.class

--- src/main/java/com/example/ReviewZIP/ReviewZipApplication.java
+++ src/main/java/com/example/ReviewZIP/ReviewZipApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication()
-public class ReviewZipApplication {
+public class ReviewZipApplication(exclude = SecurityAutoConfiguration.class) {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReviewZipApplication.class, args);
