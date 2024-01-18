d06b1cac28ddf5ab03f5762e0784a06aa62d9811
hsuush <dlhyes703@naver.com>
2024-01-18 10:49:31 +0900

    feat : ApiResponse 생성

---
+++ src/main/java/com/example/ReviewZIP/global/response/exception/handler/PostsHandler.java
@@ -0,0 +1,10 @@
+package com.example.ReviewZIP.global.response.exception.handler;
+
+import com.example.ReviewZIP.global.response.code.BaseErrorCode;
+import com.example.ReviewZIP.global.response.exception.GeneralException;
+
+public class PostsHandler extends GeneralException {
+    public PostsHandler(BaseErrorCode code) {
+        super(code);
+    }
+}
---
+++ src/main/java/com/example/ReviewZIP/global/response/exception/handler/UsersHandler.java
@@ -0,0 +1,9 @@
+package com.example.ReviewZIP.global.response.exception.handler;
+
+import com.example.ReviewZIP.global.response.code.BaseErrorCode;
+import com.example.ReviewZIP.global.response.exception.GeneralException;
+
+public class UsersHandler extends GeneralException {
+    public UsersHandler(BaseErrorCode errorCode) {super(errorCode);};
+
+}
