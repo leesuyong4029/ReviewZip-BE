aa990c8f3321fca2ba71f8ad8a0b77d01e96aeca
daeng <jyjo522i@gmail.com>
2024-01-20 16:09:29 +0900

    error: ResponseDto 문제

--- src/main/java/com/example/ReviewZIP/domain/image/ImagesController.java
+++ src/main/java/com/example/ReviewZIP/domain/image/ImagesController.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,9 +15,9 @@ public class ImagesController {
     private final ImagesService imageService;
 
     @PostMapping("/users/{userId}")
-    public ApiResponse<ImageResponseDto> uploadImage(@PathVariable(name="userId") Long userId, @RequestParam("fileList") List<MultipartFile> fileList){
+    public ApiResponse<ImageResponseDto.ImageUploadListDto> uploadImage(@PathVariable(name="userId") Long userId, @RequestParam("fileList") List<MultipartFile> fileList){
         List<Images> imageList = imageService.uploadImage(fileList, userId);
-        ImageResponseDto imageResponseDto = ImageConverter.toUploadImageDto(imageList);
+        ImageResponseDto.ImageUploadListDto imageResponseDto = ImageConverter.toUploadImageDto(imageList);
         return ApiResponse.onSuccess(imageResponseDto);
     }
 }
--- src/main/java/com/example/ReviewZIP/domain/image/dto/response/ImageResponseDto.java
+++ src/main/java/com/example/ReviewZIP/domain/image/dto/response/ImageResponseDto.java
@@ -3,10 +3,13 @@ package com.example.ReviewZIP.domain.image.dto.response;
 import lombok.*;
 
 import java.util.List;
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class ImageResponseDto {
-    private List<Long> imageIds;
+    @Builder
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ImageUploadListDto {
+        private List<Long> imageIds;
+    }
 }
