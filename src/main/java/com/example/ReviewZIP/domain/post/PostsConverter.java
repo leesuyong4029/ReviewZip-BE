ee3bc93d823d66a9c0a8fd30d7cb63070123f8b0
이혜수 <dlhyes703@naver.com>
2024-01-20 16:14:37 +0900

    feat : 게시글삭제 진행중

---
+++ src/main/java/com/example/ReviewZIP/domain/post/PostsConverter.java
@@ -0,0 +1,5 @@
+package com.example.ReviewZIP.domain.post;
+
+public class PostsConverter {
+
+}
---
+++ src/main/java/com/example/ReviewZIP/domain/post/PostsService.java
@@ -0,0 +1,10 @@
+package com.example.ReviewZIP.domain.post;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostsService {
+
+}
