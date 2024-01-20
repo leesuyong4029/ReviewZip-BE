befa9499d77c46f2256b22bc02becd5b6620b20c
이혜수 <dlhyes703@naver.com>
2024-01-20 15:36:22 +0900

    chore : 뛰어쓰기 변경

--- src/main/java/com/example/ReviewZIP/domain/user/UsersService.java
+++ src/main/java/com/example/ReviewZIP/domain/user/UsersService.java
@@ -3,7 +3,6 @@ package com.example.ReviewZIP.domain.user;
 
 import com.example.ReviewZIP.domain.post.Posts;
 import com.example.ReviewZIP.domain.post.PostsRepository;
-import com.example.ReviewZIP.domain.postLike.PostLikesRepository;
 import com.example.ReviewZIP.global.response.code.resultCode.ErrorStatus;
 import com.example.ReviewZIP.global.response.exception.handler.UsersHandler;
 import lombok.RequiredArgsConstructor;
@@ -11,8 +10,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,7 +20,6 @@ public class UsersService {
     private final PostsRepository postsRepository;
 
     @Transactional
-
     public Page<Posts> getOtherPostList(Long userId, Integer size, Integer page){
         Users user = usersRepository.findById(userId).orElseThrow(()->new UsersHandler(ErrorStatus.USER_NOT_FOUND));
         Page<Posts> UserPage = postsRepository.findAllByUser(user, PageRequest.of(page, size));
