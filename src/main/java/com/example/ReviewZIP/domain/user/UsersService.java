e833641a124cfa71e1255d481408da3d6ac3a3ee
이혜수 <dlhyes703@naver.com>
2024-01-21 15:20:30 +0900

    refactor : 매핑이용한 코드 간결화

--- src/main/java/com/example/ReviewZIP/domain/user/UsersService.java
+++ src/main/java/com/example/ReviewZIP/domain/user/UsersService.java
@@ -1,9 +1,7 @@
 package com.example.ReviewZIP.domain.user;
 
-import com.example.ReviewZIP.domain.post.Posts;
 import com.example.ReviewZIP.domain.post.PostsRepository;
 import com.example.ReviewZIP.domain.postLike.PostLikesRepository;
-import com.example.ReviewZIP.domain.scrab.Scrabs;
 import com.example.ReviewZIP.domain.scrab.ScrabsRepository;
 import com.example.ReviewZIP.global.response.code.resultCode.ErrorStatus;
 import com.example.ReviewZIP.global.response.exception.handler.UsersHandler;
@@ -23,16 +21,6 @@ public class UsersService {
     @Transactional
     public void deleteUser(Long userId){
         Users user = usersRepository.findById(userId).orElseThrow(()->new UsersHandler(ErrorStatus.USER_NOT_FOUND));
-
-        postLikesRepository.deleteById(userId);
-
-        for (Scrabs scrab : user.getScrabList()) {
-            scrabsRepository.delete(scrab);
-        }
-
-        for (Posts post : user.getPostList()) {
-            postsRepository.delete(post);
-        }
 
         usersRepository.deleteById(userId);
 
