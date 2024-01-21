78e0cb8d038eae8315461141612cf7be08a2a250
이혜수 <dlhyes703@naver.com>
2024-01-21 13:04:04 +0900

    chore : 사용하지않는 repository문 삭제

--- src/main/java/com/example/ReviewZIP/domain/user/UsersService.java
+++ src/main/java/com/example/ReviewZIP/domain/user/UsersService.java
@@ -1,16 +1,12 @@
 package com.example.ReviewZIP.domain.user;
 
-import com.example.ReviewZIP.domain.image.Images;
 import com.example.ReviewZIP.domain.image.ImagesRepository;
 import com.example.ReviewZIP.domain.post.Posts;
 import com.example.ReviewZIP.domain.post.PostsRepository;
-import com.example.ReviewZIP.domain.postHashtag.PostHashtags;
 import com.example.ReviewZIP.domain.postHashtag.PostHashtagsRepository;
-import com.example.ReviewZIP.domain.postLike.PostLikes;
 import com.example.ReviewZIP.domain.postLike.PostLikesRepository;
 import com.example.ReviewZIP.domain.scrab.Scrabs;
 import com.example.ReviewZIP.domain.scrab.ScrabsRepository;
-import com.example.ReviewZIP.global.response.code.BaseErrorCode;
 import com.example.ReviewZIP.global.response.code.resultCode.ErrorStatus;
 import com.example.ReviewZIP.global.response.exception.handler.UsersHandler;
 import lombok.RequiredArgsConstructor;
@@ -24,9 +20,7 @@ public class UsersService {
     private final UsersRepository usersRepository;
     private final PostsRepository postsRepository;
     private final ScrabsRepository scrabsRepository;
-    private final ImagesRepository imagesRepository;
     private final PostLikesRepository postLikesRepository;
-    private final PostHashtagsRepository postHashtagsRepository;
 
     @Transactional
     public void deleteUser(Long userId){
