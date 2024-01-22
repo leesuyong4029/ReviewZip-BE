564b60ee50713218a72978a07e27d257b7e4cbea
이혜수 <119154537+hsuush@users.noreply.github.com>
2024-01-22 11:58:53 +0900

    refactor : UsersController 오타 수정

--- src/main/java/com/example/ReviewZIP/domain/user/UsersController.java
+++ src/main/java/com/example/ReviewZIP/domain/user/UsersController.java
@@ -5,7 +5,6 @@ import com.example.ReviewZIP.domain.post.Posts;
 import com.example.ReviewZIP.domain.postLike.PostLikesRepository;
 import com.example.ReviewZIP.domain.user.dto.response.UserResponseDto;
 import com.example.ReviewZIP.global.response.ApiResponse;
-import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +17,21 @@ public class UsersController {
     private final UsersService usersService;
 
     @GetMapping("/{userId}/posts")
-    @ApiOperation(value = "특정 유저의 게시글 가져오기")
     public ApiResponse<UserResponseDto.PostPreviewListDto> getOtherPostList(@PathVariable(name = "userId") Long userId
             , @RequestParam(name = "size") Integer size , @RequestParam(name = "page") Integer page){
         Page<Posts> UserPage = usersService.getOtherPostList(userId, size, page);
 
         return ApiResponse.onSuccess(UsersConverter.toPostPreviewListDto(UserPage));
 
+    }
+
+     @GetMapping("/{userId}/posts/scrabs")
+    public ApiResponse<UserResponseDto.PostPreviewListDto> getOtherPostList(@PathVariable(name = "userId") Long userId, @RequestParam(name = "page")Integer page, @RequestParam(name = "size") Integer size){
+        Page<Scrabs> UserPage = usersService.getOtherScrabList(userId,page, size);
+
+        if(UserPage.isEmpty()){
+            throw new PostsHandler(ErrorStatus.SCRAB_LIST_NOT_FOUND);
+        }
+        return ApiResponse.onSuccess(UsersConverter.toPostPreviewListDto(UserPage));
     }
 }
