b409f1dc558223e9defe5806d55eb1fd88b9e3a0
YPYP333YPYP <ypyp333ypyp@gmail.com>
2024-01-28 15:56:36 +0900

    feat : 게시물 안에서 해시태그 추가하기

--- src/main/java/com/example/ReviewZIP/global/redis/RedisService.java
+++ src/main/java/com/example/ReviewZIP/global/redis/RedisService.java
@@ -1,9 +1,26 @@
 package com.example.ReviewZIP.global.redis;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class RedisService {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public void addHashtag(String query) {
+        HashOperations<String, String, String> hashOperations = redisTemplate.opsForHash();
+
+        if (hashOperations.hasKey("hashtags", query)) {
+            hashOperations.increment("hashtags", query, 1);
+        } else {
+            hashOperations.put("hashtags", query, "1");
+        }
+    }
+
 }
