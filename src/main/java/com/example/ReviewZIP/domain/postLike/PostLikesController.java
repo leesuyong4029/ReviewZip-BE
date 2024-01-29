02ea7e74428b11a0e0b381035584871c0895f716
YPYP333YPYP <ypyp333ypyp@gmail.com>
2024-01-29 17:17:37 +0900

    refactor : swagger 설명 추가, 예외 처리 추가

--- src/main/java/com/example/ReviewZIP/domain/postLike/PostLikesController.java
+++ src/main/java/com/example/ReviewZIP/domain/postLike/PostLikesController.java
@@ -6,6 +6,12 @@ import com.example.ReviewZIP.domain.user.Users;
 import com.example.ReviewZIP.domain.user.UsersConverter;
 import com.example.ReviewZIP.domain.user.dto.response.UserResponseDto;
 import com.example.ReviewZIP.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
@@ -17,10 +23,19 @@ public class PostLikesController {
 
     private final PostLikesService postLikesService;
 
-
     @GetMapping("/{postId}/users")
-    public ApiResponse<UserResponseDto.UserPreviewListDto> getUsersByPostIdFromPostLikes(@PathVariable Long postId, @RequestParam (defaultValue = "0") Integer page) {
-        Page<Users> pageUsers = postLikesService.getUsersByPostId(postId, page);
+    @Operation(summary = "게시물에 공감을 누른 유저리스트를 검색하는 API",description = "게시물 아이디를 조회하여 게시물에 공감을 누른 유저 리스트를 검색, 반환 시 UserPreviewListDto 사용")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "POSTLIKE403", description = "포스트에 공감을 한 사람이 없음",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+    })
+    @Parameters({
+            @Parameter(name = "postId", description = "게시물 아이디"),
+            @Parameter(name = "page", description = "페이지 번호"),
+            @Parameter(name = "size", description = "페이징 사이즈")
+    })
+    public ApiResponse<UserResponseDto.UserPreviewListDto> getPostLikeUserList(@PathVariable Long postId, @RequestParam (defaultValue = "0") Integer page, @RequestParam (defaultValue = "0") Integer size) {
+        Page<Users> pageUsers = postLikesService.getUsersByPostId(postId, page, size);
         UserResponseDto.UserPreviewListDto userListDto = UsersConverter.toUserListDto(pageUsers);
         return ApiResponse.onSuccess(userListDto);
     }
--- src/main/java/com/example/ReviewZIP/domain/postLike/PostLikesService.java
+++ src/main/java/com/example/ReviewZIP/domain/postLike/PostLikesService.java
@@ -1,6 +1,8 @@
 package com.example.ReviewZIP.domain.postLike;
 
 import com.example.ReviewZIP.domain.user.Users;
+import com.example.ReviewZIP.global.response.code.resultCode.ErrorStatus;
+import com.example.ReviewZIP.global.response.exception.handler.PostLikesHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -16,8 +18,12 @@ public class PostLikesService{
     private final PostLikesRepository postLikesRepository;
 
 
-    public Page<Users> getUsersByPostId(Long postId, Integer page) {
-        Page<PostLikes> postLikesList = postLikesRepository.findAllByPostId(postId, PageRequest.of(page, 10));
+    public Page<Users> getUsersByPostId(Long postId, Integer page, Integer size) {
+        Page<PostLikes> postLikesList = postLikesRepository.findAllByPostId(postId, PageRequest.of(page, size));
+
+        if (postLikesList.isEmpty()) {
+            throw new PostLikesHandler(ErrorStatus.POSTLIKE_NOT_FOUND);
+        }
         return new PageImpl<>(
                 postLikesList.getContent().stream()
                         .map(PostLikes::getUser)
