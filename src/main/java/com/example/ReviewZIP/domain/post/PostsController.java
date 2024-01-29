66e131e9244f9ca6ea039bde8b5bbec1836040f8
YPYP333YPYP <ypyp333ypyp@gmail.com>
2024-01-29 17:31:16 +0900

    refactor : swagger 추가, 예외 처리 추가

--- src/main/java/com/example/ReviewZIP/domain/post/PostsController.java
+++ src/main/java/com/example/ReviewZIP/domain/post/PostsController.java
@@ -2,6 +2,12 @@ package com.example.ReviewZIP.domain.post;
 
 import com.example.ReviewZIP.domain.post.dto.response.PostResponseDto;
 import com.example.ReviewZIP.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +20,18 @@ public class PostsController {
     private final PostsService postsService;
 
     @GetMapping("/{hashtagId}")
-    public ApiResponse<PostResponseDto.PostPreviewListDto> searchPostsByHashtagId(@PathVariable Long hashtagId, @RequestParam(defaultValue = "0") Integer page) {
-        Page<Posts> postsPage = postsService.searchPostByHashtag(hashtagId, page);
+    @Operation(summary = "해시태그 아이디로 게시글을 찾는 API",description = "해시태그 아이디로 게시글을 찾는 기능, 반환 시 PostPreviewListDto, PostPreviewDto 사용")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "HASHTAG401", description = "해시태그를 찾을 수 없음",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+    })
+    @Parameters({
+            @Parameter(name = "hashtagId", description = "해시태그 아이디"),
+            @Parameter(name = "page", description = "페이지 번호"),
+            @Parameter(name = "size", description = "페이징 사이즈")
+    })
+    public ApiResponse<PostResponseDto.PostPreviewListDto> searchPostsByHashtagId(@PathVariable Long hashtagId, @RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "0") Integer size) {
+        Page<Posts> postsPage = postsService.searchPostByHashtag(hashtagId, page, size);
         PostResponseDto.PostPreviewListDto postPreviewListDto = PostsConverter.toPostPreviewListDto(postsPage);
         return ApiResponse.onSuccess(postPreviewListDto);
     }
--- src/main/java/com/example/ReviewZIP/domain/post/PostsService.java
+++ src/main/java/com/example/ReviewZIP/domain/post/PostsService.java
@@ -2,6 +2,8 @@ package com.example.ReviewZIP.domain.post;
 
 import com.example.ReviewZIP.domain.postHashtag.PostHashtags;
 import com.example.ReviewZIP.domain.postHashtag.PostHashtagsRepository;
+import com.example.ReviewZIP.global.response.code.resultCode.ErrorStatus;
+import com.example.ReviewZIP.global.response.exception.handler.PostHashtagsHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -16,9 +18,12 @@ public class PostsService {
 
     private final PostHashtagsRepository postHashtagsRepository;
 
-    public Page<Posts> searchPostByHashtag (Long id, Integer page){
-        Page<PostHashtags> postHashtagsList = postHashtagsRepository.findPostHashtagsById(id, PageRequest.of(page,10));
+    public Page<Posts> searchPostByHashtag (Long id, Integer page, Integer size){
+        Page<PostHashtags> postHashtagsList = postHashtagsRepository.findPostHashtagsById(id, PageRequest.of(page,size));
 
+        if (postHashtagsList.isEmpty()) {
+            throw new PostHashtagsHandler(ErrorStatus.HASHTAG_NOT_FOUND);
+        }
         List<Posts> postsList = postHashtagsList.getContent().stream()
                 .map(PostHashtags::getPost)
                 .collect(Collectors.toList());
