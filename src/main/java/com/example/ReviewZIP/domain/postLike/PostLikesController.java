68a6dd9e937c0f0b532b53ea6df0d65f149a668f
YPYP333YPYP <ypyp333ypyp@gmail.com>
2024-01-29 16:55:03 +0900

    refactor : swagger 추가

--- src/main/java/com/example/ReviewZIP/domain/postLike/PostLikesController.java
+++ src/main/java/com/example/ReviewZIP/domain/postLike/PostLikesController.java
@@ -2,6 +2,12 @@ package com.example.ReviewZIP.domain.postLike;
 
 import com.example.ReviewZIP.global.response.ApiResponse;
 import com.example.ReviewZIP.global.response.code.resultCode.SuccessStatus;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +21,15 @@ public class PostLikesController {
 
 
     @DeleteMapping("/{postId}")
-    public ApiResponse<SuccessStatus> postRemoveLike(@PathVariable Long postId) {
+    @Operation(summary = "게시글에 공감 해제 API",description = "게시글에 공감을 해제하는 기능, DTO 사용 X")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "POSTLIKE403", description = "게시글에 공감이 존재하지 않음",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+    })
+    @Parameters({
+            @Parameter(name = "postId", description = "게시글 아이디"),
+    })
+    public ApiResponse<SuccessStatus> removePostLike(@PathVariable Long postId) {
         postLikesService.removeLike(postId, 1L);
         return ApiResponse.onSuccess(SuccessStatus.POST_CANCEL_LIKE_SUCCESS);
     }
