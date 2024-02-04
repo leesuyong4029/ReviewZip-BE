b97dbe41272112a4aa3b02e04b26fc09c9289a64
이혜수 <dlhyes703@naver.com>
2024-02-04 23:51:15 +0900

    feat : 유저 및 해시태그 검색 기록 저장 및 삭제

---
+++ src/main/java/com/example/ReviewZIP/domain/searchHistory/SearchHistoriesService.java
@@ -0,0 +1,62 @@
+package com.example.ReviewZIP.domain.searchHistory;
+
+import com.example.ReviewZIP.domain.user.Users;
+import com.example.ReviewZIP.domain.user.UsersRepository;
+import com.example.ReviewZIP.global.response.code.resultCode.ErrorStatus;
+import com.example.ReviewZIP.global.response.exception.handler.UsersHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class SearchHistoriesService {
+    private final UsersRepository usersRepository;
+    private final SearchHistoriesRepository searchHistoriesRepository;
+
+    @Transactional
+    public void saveUserSearchHistory(Long userId, Long objectId){
+        Users me = usersRepository.getById(1L);
+        Users object = usersRepository.findById(objectId).orElseThrow(()->new UsersHandler(ErrorStatus.USER_NOT_FOUND));
+
+        SearchHistories userHistory = SearchHistories.builder()
+                .type(SearchType.USER)
+                .user(me)
+                .object(object)
+                .hashtag(null)
+                .build();
+
+        searchHistoriesRepository.save(userHistory);
+    }
+
+    @Transactional
+    public void saveHashtagSearchHistory(Long userId, String hashtag){
+        Users me = usersRepository.getById(1L);
+        SearchHistories hashtagHistory = SearchHistories.builder()
+                .type(SearchType.HASHTAG)
+                .user(me)
+                .object(null)
+                .hashtag(hashtag)
+                .build();
+
+        searchHistoriesRepository.save(hashtagHistory);
+    }
+
+    @Transactional
+    public void deleteUserSearchHistory(Long userId, Long objectId){
+        Users me = usersRepository.getById(userId);
+        Users object = usersRepository.findById(objectId).orElseThrow(()->new UsersHandler(ErrorStatus.USER_NOT_FOUND));
+        SearchHistories history = searchHistoriesRepository.findByUserAndObjectId(me, object);
+        searchHistoriesRepository.delete(history);
+    }
+
+    @Transactional
+    public void deleteHashtagSearchHistory(Long userId, String hashtag){
+        Users me = usersRepository.getById(userId);
+
+        SearchHistories history = searchHistoriesRepository.findByUserAndHashtag(me, hashtag);
+
+        searchHistoriesRepository.delete(history);
+    }
+}
--- src/main/java/com/example/ReviewZIP/global/response/code/resultCode/ErrorStatus.java
+++ src/main/java/com/example/ReviewZIP/global/response/code/resultCode/ErrorStatus.java
@@ -58,7 +58,10 @@ public enum ErrorStatus implements BaseErrorCode {
     IMAGE_UPLOAD_FAIL(HttpStatus.BAD_REQUEST, "IMAGE402", "이미지 업로드에 실패하였습니다."),
 
     // Follow
-    FOLLOW_ALREADY(HttpStatus.BAD_REQUEST, "FOLLOW401", "이미 팔로우한 상태입니다.");
+    FOLLOW_ALREADY(HttpStatus.BAD_REQUEST, "FOLLOW401", "이미 팔로우한 상태입니다."),
+
+    // SearchHistory
+    HISTORY_NOT_FOUNT(HttpStatus.NOT_FOUND, "HISTORY401", "해당하는 기록이 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
