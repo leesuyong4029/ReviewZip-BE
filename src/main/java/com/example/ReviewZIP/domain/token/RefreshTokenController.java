fea5d1992d8fad79c4cbf96e3e46ffd960be46e5
suyong <edgarlee9364@gmail.com>
2024-02-11 15:50:38 +0900

    fix: 로그인, 회원가입 코드 Swagger 적용

--- src/main/java/com/example/ReviewZIP/domain/token/RefreshTokenController.java
+++ src/main/java/com/example/ReviewZIP/domain/token/RefreshTokenController.java
@@ -1,18 +1,16 @@
 package com.example.ReviewZIP.domain.token;
 
-import com.example.ReviewZIP.domain.token.dto.LoginRequestDto;
-import com.example.ReviewZIP.domain.token.dto.SignUpRequestDto;
-import com.example.ReviewZIP.domain.token.dto.SignUpResponseDto;
-import com.example.ReviewZIP.domain.token.dto.TokenDto;
+import com.example.ReviewZIP.domain.token.dto.request.LoginRequestDto;
+import com.example.ReviewZIP.domain.token.dto.request.SignUpRequestDto;
+import com.example.ReviewZIP.domain.token.dto.response.SignUpResponseDto;
+import com.example.ReviewZIP.domain.token.dto.response.TokenDto;
+import com.example.ReviewZIP.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,30 +18,29 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "token-controller", description = "로컬 로그인, 회원가입 API")
 @RestController
-@RequestMapping("/v1/auth")
+@RequestMapping("v1/auth")
 @RequiredArgsConstructor
 public class RefreshTokenController {
     private final RefreshTokenService refreshTokenService;
 
     @PostMapping("/local/sign-up")
-    @Operation(summary = "로컬 회원가입", description = "JWT 이용해 로컬 회원가입 진행")
+    @Operation(summary = "로컬 회원가입", description = "JWT 이용해 로컬 회원가입 진행, SignUpRequestDto, SignUpResponseDto 사용")
     @ApiResponses({
-            @ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
-            @ApiResponse(responseCode = "USER401", description = "이미 존재하는 이메일입니다.", content = @Content(schema = @Schema(implementation = ApiResponse.class)))
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "USER401", description = "이미 존재하는 이메일입니다.", content = @Content(schema = @Schema(implementation = ApiResponse.class)))
     })
-    public ResponseEntity<SignUpResponseDto> signUp(@RequestBody SignUpRequestDto signUpRequestDto) {
+    public ApiResponse<SignUpResponseDto> signUp(@RequestBody SignUpRequestDto signUpRequestDto) {
         SignUpResponseDto savedUserDto = refreshTokenService.signUp(signUpRequestDto);
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(savedUserDto);
+        return ApiResponse.onSuccess(savedUserDto);
     }
 
     @PostMapping("/local/login")
     @Operation(summary = "로컬 로그인", description = "JWT 이용해 로컬 로그인 진행")
     @ApiResponses({
-            @ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
-            @ApiResponse(responseCode = "USER403", description = "비밀번호가 잘못되었습니다.")
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "USER403", description = "비밀번호가 잘못되었습니다.")
     })
-    public ResponseEntity<TokenDto> login(@RequestBody LoginRequestDto loginRequestDto) {
-        return ResponseEntity.ok(refreshTokenService.login(loginRequestDto));
+    public ApiResponse<TokenDto> login(@RequestBody LoginRequestDto loginRequestDto) {
+        return ApiResponse.onSuccess(refreshTokenService.login(loginRequestDto));
     }
 }
