17686ea5542b6ae9e9cbb7c652d8f647bb4a707f
이혜수 <dlhyes703@naver.com>
2024-02-13 20:15:12 +0900

    feat : 카카오 로그인 테스트

--- src/main/java/com/example/ReviewZIP/domain/token/RefreshTokenController.java
+++ src/main/java/com/example/ReviewZIP/domain/token/RefreshTokenController.java
@@ -1,10 +1,12 @@
 package com.example.ReviewZIP.domain.token;
 
+import com.example.ReviewZIP.domain.token.dto.request.KakaoRequestDto;
 import com.example.ReviewZIP.domain.token.dto.request.LoginRequestDto;
 import com.example.ReviewZIP.domain.token.dto.request.SignUpRequestDto;
 import com.example.ReviewZIP.domain.token.dto.response.TokenDto;
 import com.example.ReviewZIP.global.response.ApiResponse;
 import com.example.ReviewZIP.global.response.code.resultCode.SuccessStatus;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Tag(name = "token-controller", description = "로컬 로그인, 회원가입 API")
 @RestController
@@ -42,5 +46,11 @@ public class RefreshTokenController {
     })
     public ApiResponse<TokenDto> login(@RequestBody LoginRequestDto loginRequestDto) {
         return ApiResponse.onSuccess(refreshTokenService.login(loginRequestDto));
+    }
+
+    @PostMapping("/kakao/login")
+    public ApiResponse<TokenDto> kakaoLogin(@RequestBody KakaoRequestDto request) throws JsonProcessingException {
+        List<String> kakaoUserInfo = refreshTokenService.getKakaoUserInfo(request);
+        return ApiResponse.onSuccess(refreshTokenService.kakaoLogin(kakaoUserInfo));
     }
 }
--- src/main/java/com/example/ReviewZIP/domain/token/RefreshTokenService.java
+++ src/main/java/com/example/ReviewZIP/domain/token/RefreshTokenService.java
@@ -1,5 +1,6 @@
 package com.example.ReviewZIP.domain.token;
 
+import com.example.ReviewZIP.domain.token.dto.request.KakaoRequestDto;
 import com.example.ReviewZIP.domain.token.dto.request.LoginRequestDto;
 import com.example.ReviewZIP.domain.token.dto.request.SignUpRequestDto;
 import com.example.ReviewZIP.domain.token.dto.response.SignUpResponseDto;
@@ -10,13 +11,26 @@ import com.example.ReviewZIP.global.jwt.JwtProvider;
 import com.example.ReviewZIP.global.response.code.resultCode.ErrorStatus;
 import com.example.ReviewZIP.global.response.exception.GeneralException;
 import com.example.ReviewZIP.global.security.UserDetailsImpl;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -67,4 +81,80 @@ public class RefreshTokenService {
         return tokenDto;
     }
 
+    @Transactional
+    public Long createUser(String id, String nickname, String email, String password){
+        Users newUser = Users.builder()
+                .social(id)
+                .nickname(nickname)
+                .name(nickname)
+                .email(email)
+                .password(password)
+                .build();
+        usersRepository.save(newUser);
+
+        return newUser.getId();
+    }
+
+    public List<String> getKakaoUserInfo(KakaoRequestDto request) throws JsonProcessingException {
+        String token = request.getToken();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+        headers.set("Authorization", "Bearer " + token);
+
+        HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest = new HttpEntity<>(null, headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                "https://kapi.kakao.com/v2/user/me",
+                HttpMethod.POST,
+                kakaoProfileRequest,
+                String.class
+        );
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String responseBody = response.getBody();
+        JsonNode jsonNode = objectMapper.readTree(responseBody);
+        String id = jsonNode.get("id").asText();
+        String nickname = jsonNode.get("properties").get("nickname").asText();
+        String email = jsonNode.get("kakao_account").get("email").asText();
+
+        List<String> kakaoUserInfoList = Arrays.asList(id, nickname, email);
+
+        return kakaoUserInfoList;
+    }
+
+    public TokenDto kakaoLogin(List<String> kakaoUserInfo) {
+        // 1. Login ID/PW를 기반으로 AuthenticationToken 생성
+        Long userIdd;
+        Optional<Users> user = usersRepository.findByEmail(kakaoUserInfo.get(2));
+
+        String ppassword = "dfjdfaejfkdasjfo2324akdjfiejf";
+        String password = encodePassword(ppassword);
+
+
+        if(!user.isPresent()){
+            userIdd = createUser(kakaoUserInfo.get(0), kakaoUserInfo.get(1), kakaoUserInfo.get(2), password);
+        }
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(kakaoUserInfo.get(2), ppassword);
+
+        // 2. authentication이 실행이 될 때, UserDetailsServiceImpl 에서 만들었던 loadUserByUsername 메서드가 실행됨
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        // 3. UserDetailsImpl에서 직접 userId 가져오기
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        Long userId = userDetails.getUserId();
+
+        // 4. 인증 정보를 기반으로 JWT 토큰 생성
+        TokenDto tokenDto = jwtProvider.generateToken(authentication, userId.toString());
+        // 5. RefreshToken 저장
+        RefreshToken refreshToken = RefreshToken.builder()
+                .key(authentication.getName())
+                .value(tokenDto.getRefreshToken())
+                .build();
+
+        refreshTokenRepository.save(refreshToken);
+        return tokenDto;
+    }
 }
