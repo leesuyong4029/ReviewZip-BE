6fd149b3b8762b414d2ebecacff667904bc2ac8b
suyong <edgarlee9364@gmail.com>
2024-02-17 17:05:02 +0900

    chore: username 코드 수정

--- src/main/java/com/example/ReviewZIP/global/jwt/JwtProvider.java
+++ src/main/java/com/example/ReviewZIP/global/jwt/JwtProvider.java
@@ -94,7 +94,7 @@ public class JwtProvider {
                         .collect(Collectors.toList());
 
         // UserDetails 객체를 만들어서 Authentication 리턴
-        UserDetails principal = new User(claims.getSubject(), "", authorities);
+        UserDetails principal = new User(username, "", authorities);
 
         // 추출된 userId를 SecurityContextHolder에 저장
         SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(principal, "", authorities));
